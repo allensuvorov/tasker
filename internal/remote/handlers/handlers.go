e091091f4bd0ae0788c91fa1213f16ecf85fb582
Allen <allenvs@mail.ru>
2022-12-09 20:50:39 +0600

    CreateTask logs

--- internal/remote/handlers/handlers.go
+++ internal/remote/handlers/handlers.go
@@ -20,6 +20,7 @@ func NewTaskHandler(ts TaskService) TaskHandler {
 
 func (th TaskHandler) CreateTask(w http.ResponseWriter, r *http.Request) {
 	log.Println("Handler CreateTask - hello")
+	log.Println("Handler CreateTask - bye")
 }
 
 func (th TaskHandler) GetStatus(w http.ResponseWriter, r *http.Request) {
