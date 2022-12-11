8632470e42efe97476a93d666d344e09add9f6d7
Allen <allenvs@mail.ru>
2022-12-11 21:17:46 +0600

    draft interfaces for doer-scheduler

--- cmd/scheduler/scheduler.go
+++ cmd/scheduler/scheduler.go
@@ -1,5 +1,9 @@
 package scheduler
 
+import (
+	"github.com/allensuvorov/tasker/internal/domain"
+)
+
 func schedule(taskID string) {
 	// TODO: task scheduler with goroutines
 	// TODO: go func(){}() that assigns task to agent - taskAgent(taskID)
@@ -12,6 +16,31 @@ func taskAgent(taskID string) {
 	//update status of task in DB
 }
 
+type DoService interface {
+	GetNewTasks() []domain.TaskEntity
+	ScheduleTasks([]domain.TaskEntity) []domain.ResultEntity
+	BulkCreateTaskResults(results []domain.ResultEntity)
+}
+
+type DoStorage interface {
+	GetNewTasks() []domain.TaskEntity
+	BulkUpdateTaskStatuses(map[string]int)
+	BulkCreateTaskResults(results []domain.ResultEntity)
+}
+
+type DoRequest interface {
+	Request(domain.TaskEntity) domain.ResultEntity
+}
+
 func main() {
-	schedule("taskID")
+	// TODO: check DB for new tasks
+	// TODO: send tasks to 3rdparty
+	// TODO: update DB
+	// TODO: add layers for - storage, service, remote
+
+	//DoStorage := NewDoStorage()
+	//DoRemote := NewDoRemote()
+	//DoService := NewDoService(DoRemote, DoStorage)
+	//reCheckTime := 500*time.Millisecond
+	//DoService.doStorage.GetNewTasks(reCheckTime)
 }
